8b1ef49ca4c79dfcad8e692f7c4f760614267e51
diegomartinmoreno <39862933+diegomartinmoreno@users.noreply.github.com>
2018-11-11 01:11:45 -0300

    Add files via upload

---
+++ FuncionesEstadisticas.c
@@ -0,0 +1,148 @@
+#include "headers.h"
+
+void imprimirMenuEstadisticas (){
+    imprimirHeader("     Estadisticas     ");
+    textcolor(10);
+    printf("\n\t1.- ");
+    textcolor(15);
+    printf("Historico de averias por camara.");
+    textcolor(10);
+    printf("\n\t2.- ");
+    textcolor(15);
+    printf("Historico de alertas por cliente.");
+    textcolor(10);
+    printf("\n\t3.- ");
+    textcolor(15);
+    printf("Promedio de tiempo de inactividad por camara.");
+    textcolor(10);
+    printf("\n\t4.- ");
+    textcolor(15);
+    printf("Promedio de alertas/averias por supervisor.");
+    textcolor(10);
+    printf("\n\t5.- ");
+    textcolor(15);
+    printf("Promedio de alertas/averias por periodo.");
+    textcolor(10);
+    printf("\n\t6.- ");
+    textcolor(15);
+    printf("Tiempo medio de respuesta alertas/averias.");
+}
+
+void procesarMenuEstadisticas(arbolCamara *arbol){
+    char control='n', op;
+    while (control!='s'&&control!='S'){
+        imprimirMenuEstadisticas();
+        fflush(stdin);
+        op=getch();
+        switch(op){
+        case '1':
+            //histAverias();
+            break;
+        case '2':
+            //histAlertas();
+            break;
+        case '3':
+            //promInactividad();
+            break;
+        case '4':
+            //promSupervisor();
+            break;
+        case '5':
+            //promPeriodo();
+            break;
+        case '6':
+            //promRespuesta();
+            break;
+        default:
+            puts("Opcion incorrecta!");
+            break;
+        }
+    }
+}
+
+/// PROMEDIOS
+/// Cantidad de averias por camara.
+/// Cantidad de alertas por cliente.
+/// Promedio de tiempo inactiva.
+/// Promedio de alertas por supervisor.
+/// Promedio de averias por supervisor.
+/// Promedio de alertas/averias totales en un periodo de tiempo.
+/// Tiempo medio respuesta alertas.
+/// Tiempo medio respuesta averias.
+
+typedef struct
+{
+    char cli[sizeNom];
+    struct listaClientes *sig;
+} listaClientes;
+
+
+int comprobarRepetido (listaClientes *primero, char cliente[sizeNom]) { /// Retorna 0 si ya tengo ese cliente y 1 si no lo tengo.
+    listaClientes *lista=primero;
+    int rta=1;
+    while (lista){
+        if (strcmp(lista->cli, cliente)==0){
+            rta=0;
+        }
+        lista=lista->sig;
+    }
+    return rta;
+}
+
+void agregarFinal(listaClientes **clientes, char agregar []){
+    listaClientes *seg=*clientes;
+    if (*clientes){
+        while (seg->sig!=NULL){
+            seg=seg->sig;
+        }
+        seg->sig=(listaClientes*)malloc(sizeof(listaClientes));
+        seg=seg->sig;
+        strcpy(seg->cli, agregar);
+    }else{
+    *clientes=(listaClientes*)malloc(sizeof(listaClientes));
+    strcpy((*clientes)->cli, agregar);
+    }
+}
+
+listaClientes * obtenerClientes (arbolCamara *arbol, listaClientes *lista){
+    celda aux;
+    aux=arbol->C;
+    lugar aux2;
+    aux2=aux.ubicacion; /// Gracias por tanto Code::Blocks
+    if (arbol){
+        lista=obtenerClientes(arbol->izquierda, lista);
+        lista=obtenerClientes(arbol->derecha, lista);
+        if (comprobarRepetido(lista, aux2.nombre)){
+            agregarFinal(&lista, aux2.nombre);
+        }
+    }
+    return lista;
+}
+
+int obtenerInput (char input[], int *numero){ /// Retorna 1 si se ingreso un ID y 0 si se ingreso una palabra.
+    int i=0, rta=0;
+    fflush(stdin);
+    while (stdin){
+        input[i]=getch;
+        i++;
+    }
+    input [i]='\0';
+    if (47<(input[0])&& (input[0])<58) { /// Es numero
+        *numero=atoi(input);
+        rta=1;
+    }
+    return rta;
+}
+
+void histAverias(arbolCamara *arbol){
+    system("cls");
+    char input[sizeNom];
+    int esNumero, ID;
+    imprimirHeader(" Historico de Averias ");
+    puts("Ingrese el nombre del cliente o el ID de la camara buscada:");
+    esNumero=obtenerInput(input, &ID);
+    if (esNumero){
+        ///obtenerHist(ID);
+    }
+
+}
---
+++ FuncionesSupervisor.c
@@ -0,0 +1,192 @@
+#include "headers.h"
+/// *** Funciones por si se puede ingresar una entrada de historial de fechas anteriores, ordenandola antes de las ultimas entradas.
+
+int compararFechas (tiempo t1, tiempo t2){ /// 1 si son iguales, -1 si tienen mismo dia distinta hora. 2 si t1 es mayor y -2 si t2 es mayor.
+    int rta=-2;
+    if (t1.ano==t2.ano && t1.mes==t2.mes && t1.dia==t2.dia){ /// Compara fecha
+        rta=-1;
+    }
+    if (t1.hora==t2.hora && t1.minuto==t2.minuto && t1.segundo==t2.segundo){ /// Comprara hora
+        rta=1;
+    }
+    if (t1.ano>t2.ano){
+        rta=2;
+    }else {
+        if (t1.mes>t2.mes){
+            rta=2;
+        }else {
+            if (t1.dia>t2.dia){
+                rta=2;
+            }else {
+                if (t1.hora>t2.hora){
+                    rta=2;
+                }else{
+                    if (t1.minuto>t2.minuto){
+                        rta=2;
+                    }
+                    else{
+                        if (t1.segundo>t2.segundo)
+                            rta=2;
+                    }
+                }
+            }
+        }
+    }
+    return rta;
+}
+
+
+historial *insertarOrdenardoAA (historial *lista, historial *nuevo){
+    historial *seg=lista, *anterior=lista;
+    if (lista){
+        if (compararFechas(seg->fecha, nuevo->fecha)==-2){
+            while (seg->siguiente && compararFechas(seg->fecha, nuevo->fecha)==2){
+                anterior=seg;
+                seg=seg->siguiente;
+            }
+            anterior->siguiente=nuevo;
+            nuevo->siguiente=seg;
+        }else{
+            nuevo->siguiente=lista;
+            lista=nuevo;
+        }
+    }else{
+        lista=nuevo;
+    }
+    return lista;
+}
+
+///****************************************
+
+int realocarArreglo (celda camaras[], int dimF){
+    camaras=realloc(camaras, sizeof(celda)+10);
+    if (!camaras)
+        perror("No se encontro espacio suficiente para realocar arreglo de camaras.");
+    return dimF;
+}
+
+
+int generarArregloCamaras (arbolCamara *arbol, celda camaras[], int dimL, int dimF, char usuario[]){
+    if (arbol){
+        if (strcmp(usuario, arbol->C.supervisor)==0){
+           if (dimL+1>dimF){
+                dimF=realocarArreglo(camaras, dimF);
+           }
+           camaras[dimL]=arbol->C;
+           dimL++;
+        }
+        dimL=generarArregloCamaras(arbol->izquierda, camaras, dimL, dimF, usuario);
+        dimL=generarArregloCamaras(arbol->derecha, camaras, dimL, dimF, usuario);
+    }
+    return dimL;
+}
+
+
+char cicloDeControl (){
+    char rta=0;
+    int i=0;
+    while (i<14 && rta==0){
+        fflush(stdin);
+        Sleep(500);
+        if (!feof(stdin)){
+            rta=getch();
+        }
+    }
+    return rta;
+}
+
+
+void inicSupervision (celda camaras[], int i, int dimL){
+    int prioridad=1;
+    char control='0';
+    do{
+        system("cls");
+        if (i<dimL){
+            imprimirHeadSupervisor();
+            printf ("Comienza supervision de la camara: %i", camaras[i].IDcamara);
+            mostrarUnaCamara(camaras[i]);
+            puts ("<<<<---------------------------------------------------->>>>");
+            puts ("Cambiando de camara en 7 segundos.");
+            control=cicloDeControl();
+            if (control=='1'){
+                ///ingresarAlarma(camaras[i]);
+            }else{
+                if (control =='2'){
+                    ///ingresarAveria(camaras[i]);
+                }
+            }
+        } else {
+            puts("Vigilancia finalizada. Iniciando nueva ronda en 5 segundos.");
+            i=0;
+            Sleep(5000);
+        }
+    } while (control!='3');
+}
+
+
+void procesamientoSupervision (arbolCamara *arbol, char usuario[]) {
+    celda camaras[50];
+    int dimL, dimF=50, i=0;
+    char control;
+    dimL=generarArregloCamaras(arbol, camaras, dimL, dimF, usuario);
+    while (control!='s'&&control!='S'){
+        inicSupervision(camaras, i, dimL);
+        puts("Desea cerrar sesion en este usuario?");
+        puts("S para salir.");
+        puts("N para reanudar vigilancia.");
+        fflush(stdin);
+        control=getch();
+    }
+}
+
+
+int contarUsuariosCreados (){
+    int cant;
+    FILE *dataUsuarios=fopen(rutaSupervisores, "r");
+    cant = fseek(dataUsuarios, 0, SEEK_END);
+    cant = cant/(sizeof(char)*sizeNom*2);
+    fclose(dataUsuarios);
+    return cant;
+}
+
+
+int confirmarUsuario (char usuario[]){
+    int cantUsers=contarUsuariosCreados(), i=0, encontrado=0, aprobado=0;
+    char contrasena[sizeNom], aux[sizeNom];
+    puts("Ingrese su nombre de usuario de Supervisor:");
+    fflush(stdin);
+    gets(usuario);
+    puts("Ingrese su contraseña:");
+    fflush(stdin);
+    gets(contrasena);
+    FILE *dataUsuarios=fopen(rutaSupervisores, "r");
+    while (i<cantUsers && !encontrado){
+        i++;
+        fread(aux, sizeof(char), sizeNom, dataUsuarios);
+        if (strcmp(usuario, aux)==0){
+            encontrado=1;
+            fread(aux, sizeof(char), 50, dataUsuarios);
+            if (strcmp(contrasena, aux)==0){
+                aprobado=1;
+            }
+            else{
+                puts("Contraseña incorrecta");
+            }
+        }
+    }
+    return aprobado;
+}
+
+
+void inicioSesion (arbolCamara *arbol){
+    char usuario[sizeNom];
+    int aprobado=0;
+    while (aprobado==0){
+        system("cls");
+        imprimirHeader("   Inicio de Sesion   ");
+        aprobado=confirmarUsuario(usuario);
+        if (aprobado){
+            procesamientoSupervision(arbol, usuario);
+        }
+    }
+}
